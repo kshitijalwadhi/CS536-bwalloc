04448b9164d93b417929578d36165aed5e20cc91
shreyash2106 <shreyash2106@berkeley.edu>
2023-10-26 17:13:14 -0400

    setup grpc connecttion to server

--- client.py
+++ client.py
@@ -1,11 +1,27 @@
 import argparse
 
+from imutils.video import FPS
+import time
+import grpc
 
-def send_video(client_fps, client_packet_drop_rate):
+import object_detection_pb2
+import object_detection_pb2_grpc
+
+def send_video(server_address, client_fps, client_packet_drop_rate):
+    print("Sending video")
+    channel = grpc.insecure_channel(args.server)
+    stub = object_detection_pb2_grpc.DetectorStub(channel)
+    #vs = VideoStream(src=0).start()
+    time.sleep(1.0)
+    fps = FPS().start()
     pass
 
 def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Bandwidth Allocation for Multi-Stream Video")
+    parser.add_argument('--server', 
+        default='some-server:50051', 
+        help='Server url:port'
+    )
     parser.add_argument(
         "fps",
         type=int,
@@ -25,6 +41,7 @@ def get_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args = get_args()
+    server_address = args.server
     client_fps = args.fps
     client_packet_drop_rate = args.packet_drop_rate
-    send_video(client_fps, client_packet_drop_rate)
+    send_video(server_address, client_fps, client_packet_drop_rate)
