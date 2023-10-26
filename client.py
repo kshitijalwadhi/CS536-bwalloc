fc55a43da35194d9658cb10506fd5ec586ec67ad
shreyash2106 <shreyash2106@berkeley.edu>
2023-10-26 17:27:59 -0400

    use imutils FileVideoStream instead of cv2 VideoCapture

--- client.py
+++ client.py
@@ -1,13 +1,31 @@
 import argparse
 
 from imutils.video import FPS
+from imutils.video import FileVideoStream
 import time
 import grpc
+import pickle
 
 import cv2
 
 import object_detection_pb2
 import object_detection_pb2_grpc
+
+from utils import draw_result
+
+def webcam(vs, mirror=False):
+    while True:
+        img = vs.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        # crop image to square as YOLO input
+        if img.shape[0] < img.shape[1]:
+            pad = (img.shape[1]-img.shape[0])//2
+            img = img[:, pad: pad+img.shape[0]]
+        else:
+            pad = (img.shape[0]-img.shape[1])//2
+            img = img[pad: pad+img.shape[1], :]
+        yield img
 
 def send_video(server_address, client_fps, client_packet_drop_rate):
     print("Sending video")
@@ -16,27 +34,30 @@ def send_video(server_address, client_fps, client_packet_drop_rate):
     #vs = VideoStream(src=0).start()
     time.sleep(1.0)
     fps = FPS().start()
-    cap = cv2.VideoCapture('some_video.mp4') #TODO: CHANGE THIS
-    if (cap.isOpened()== False): 
-        print("Error opening video stream or file")
-
-    while(cap.isOpened()):
-
-        ret, frame = cap.read()
-        if ret == True:
-
-            cv2.imshow('Video Frame', frame)
-            fps.update()
+    vs = FileVideoStream('some_video.mp4').start() #TODO: CHANGE THIS
+    size = 224
+    try:
+        for img in webcam(vs, mirror=True):
+            # compress frame
+            resized_img = cv2.resize(img, (size, size))
+            jpg = cv2.imencode('.jpg', resized_img)[1]
+            # send to server for object detection
+            response = stub.detect(object_detection_pb2.Image(jpeg_data=pickle.dumps(jpg)))
+            # parse detection result and draw on the frame
+            result = pickle.loads(response.data)
+            display = draw_result(img, result, scale=float(img.shape[0])/size)
+            cv2.imshow('Video Frame', display)
             wait_time = int(1000/fps)
-            if cv2.waitKey(wait_time) & 0xFF == ord('q'):
-                break
-
-        else: 
-            break
-    cap.release()
-    fps.stop()
-
-    pass
+            cv2.waitKey(wait_time)
+            fps.update()
+    except grpc._channel._Rendezvous as err:
+        print(err)
+    except KeyboardInterrupt:
+        fps.stop()
+        print('[INFO] elapsed time: {:.2f}'.format(fps.elapsed()))
+        print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
+        cv2.destroyAllWindows()
+        vs.stop()
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Bandwidth Allocation for Multi-Stream Video")
