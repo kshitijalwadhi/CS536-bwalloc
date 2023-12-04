cef4ec1a6ff8e5892a183733cc0f040fd55a5d96
shreyash2106 <shreyash2106@berkeley.edu>
2023-12-03 19:08:55 -0500

    use yolov5s

--- object_detector.py
+++ object_detector.py
@@ -1,22 +1,31 @@
 from object_detection_pb2 import BBoxes
 import random
-from utils.constants import IMG_SIZE
+from utilities.constants import IMG_SIZE
 import pickle
 import time
 
+import torch
 
 class ObjectDetector:
+    def __init__(self):
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     def detect(self, frame) -> BBoxes:
-        dummy_output = []
+        output = []
 
-        for _ in range(random.randint(1, 5)):
-            dummy_output.append(
-                ['rectangle', random.randint(0, IMG_SIZE), random.randint(0, IMG_SIZE), random.randint(10, 50), random.randint(10, 50), random.random()]
+        # for _ in range(random.randint(1, 5)):
+        #     dummy_output.append(
+        #         ['rectangle', random.randint(0, IMG_SIZE), random.randint(0, IMG_SIZE), random.randint(10, 50), random.randint(10, 50), random.random()]
+        #     )
+        result = self.model(frame)
+        score = max(result.pandas().xyxy[0].confidence) if result.pandas().xyxy[0].confidence else 100
+        for _, row in result.pandas().xyxy[0].iterrows():
+            output.append(
+                ['rectangle', row['xmin'], row['xmax'], row['ymin'], row['ymax'], row['confidence']]
             )
-        res = BBoxes(data=pickle.dumps(dummy_output))
-        if random.random() < 0.8:
-            score = random.uniform(85, 100)
-        else:
-            score = random.uniform(0, 85)
-        time.sleep(0.05)
+        res = BBoxes(data=pickle.dumps(output))
+        # if random.random() < 0.8:
+        #     score = random.uniform(85, 100)
+        # else:
+        #     score = random.uniform(0, 85)
+        # time.sleep(0.05)
         return res, score
